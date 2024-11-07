dc7da261295da8ea42f787061a0ca9ac6738c165
yejinCodes <yehi97@gmail.com>
2024-11-07 11:38:00 +0900

    slack test

--- src/main/java/com/crofle/livecrowdfunding/domain/entity/EssentialDocument.java
+++ src/main/java/com/crofle/livecrowdfunding/domain/entity/EssentialDocument.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 @Entity
 public class EssentialDocument {
