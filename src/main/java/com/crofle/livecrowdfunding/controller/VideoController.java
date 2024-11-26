5207a17ff37e29042f8b1a9b645c26453d260159
cookiemiro <xodml9598@gmail.com>
2024-11-26 12:38:49 +0900

    feat: Video recording function API & Naver object storage integration

---
+++ src/main/java/com/crofle/livecrowdfunding/controller/VideoController.java
@@ -0,0 +1,27 @@
+package com.crofle.livecrowdfunding.controller;
+
+import com.crofle.livecrowdfunding.domain.entity.Video;
+import com.crofle.livecrowdfunding.dto.response.VideoResponseDTO;
+import com.crofle.livecrowdfunding.service.VideoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recordings")
+public class VideoController {
+    private final VideoService videoService;
+
+    @PostMapping
+    public ResponseEntity<VideoResponseDTO> uploadVideo(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("scheduleId") Long scheduleId) {
+        Video video = videoService.uploadVideo(file, scheduleId);
+        return ResponseEntity.ok(new VideoResponseDTO(video.getMediaUrl()));
+    }
+}
